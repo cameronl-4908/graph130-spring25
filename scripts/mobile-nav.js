fb19ac85371e44168b763a8c10fb2639d2591b13
cameronl-4908 <cameronlarsh@gmail.com>
2025-06-13 12:29:02 -0700

    nav updates

--- scripts/mobile-nav.js
+++ scripts/mobile-nav.js
@@ -2,5 +2,5 @@ const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('kiwi')[0]
 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
+    navbarLinks.classList.toggle('active');
 })
